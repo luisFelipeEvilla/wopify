14d65ec9c13bbf38e37b201687f0553869eacc58
Luis Evilla <luisevilla588@gmail.com>
2024-01-11 14:34:20 -0500

    initial setup with redux and taiwlindcss

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+/* Components */
+import { Providers } from '@/lib/providers'
+import { Nav } from './components/Nav'
+
+/* Instruments */
+import styles from './styles/layout.module.css'
+import './styles/globals.css'
+
+export default function RootLayout(props: React.PropsWithChildren) {
+  return (
+    <Providers>
+      <html lang="en">
+        <body>
+          <section className={styles.container}>
+            <Nav />
+
+            <main className={styles.main}>{props.children}</main>
+            
+            
+          </section>
+        </body>
+      </html>
+    </Providers>
+  )
+}
