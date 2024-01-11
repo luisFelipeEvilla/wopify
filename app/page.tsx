5b7ca6877f6dab24dfaf019f34ee2a0ac080236f
Luis Evilla <luisevilla588@gmail.com>
2024-01-11 15:26:03 -0500

    show reviews count

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@
 "use client";
 import { Counter } from "./components/Counter/Counter";
 import { useState } from "react";
-import { StarIcon } from "@heroicons/react/20/solid";
 import { RadioGroup } from "@headlessui/react";
 import Image from "next/image";
 import { Accordion, AccordionItem, Button } from "@nextui-org/react";
+import { StarIcon } from '@heroicons/react/24/outline';
+
 const product = {
   name: "Basic Tee 6-Pack",
   price: "$192",
@@ -61,7 +62,7 @@ const product = {
   care: ["Machine wash cold", "Tumble dry low"],
   materials: ["100% cotton"],
   shipping: ["Arrives in 5 to 7 days"],
-  };
+};
 const reviews = { href: "#", average: 4, totalCount: 117 };
 
 export default function IndexPage() {
@@ -69,29 +70,53 @@ export default function IndexPage() {
   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-4 p-8">
       {/* Image gallery */}
-      <section className="p-8">
-        <div>
-          <img
-            src={product.images[0].src}
-            alt={product.images[0].alt}
-            width={500}
-            height={500}
-          />
+
+      <section>
+        <img
+          src={product.images[0].src}
+          alt={product.images[0].alt}
+          width={500}
+          height={500}
+        />
+      </section>
+
+      <section className="flex flex-col gap-8">
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold text-2xl">{product.name}</h3>
+
+           {
+            <div className="flex items-center gap-2">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <StarIcon
+                  key={`star-${index}-${Math.random()}`}
+                  className="w-5 h-5 text-yellow-500"
+                  fill={index < reviews.average ? "yellow" : "none"}
+                />
+              ))}
+              <span className="text-gray-500">{reviews.average}</span>
+              <span className="text-gray-500">
+                ({reviews.totalCount} reviews)
+              </span>
+            </div>
+           }
+          <p className="font-medium text-xl"> {product.price}</p>
         </div>
 
-        <div>
-          <div className="flex flex-col gap-2">
-            <h3 className="font-semibold">{product.name}</h3>
-            <p className="font-medium"> {product.price}</p>
-          </div>
+        <p>{product.description}</p>
 
-          <p>{product.description}</p>
+        <Button
+          color="success"
+          radius="sm"
+          size="lg"
+          className="text-white w-full"
+        >
+          Pay with credit card
+        </Button>
+      </section>
 
-          <Button color="success" radius="sm" size="lg" className="text-white w-full">Pay with credit card</Button>
-        </div>
-
+      <section>
         <Accordion selectionMode="multiple">
           <AccordionItem title="Features">
             <ItemList items={product.highlights} />
@@ -113,14 +138,12 @@ export default function IndexPage() {
     </div>
   );
 
-  function ItemList(props: {items: String[]}) {
+  function ItemList(props: { items: String[] }) {
     return (
       <ul className="list-disc px-4">
-        {
-          props.items.map((item,index) => (
-            <li key={`item-${index}-${Math.random()}`}>{item}</li>
-          ))
-        }
+        {props.items.map((item, index) => (
+          <li key={`item-${index}-${Math.random()}`}>{item}</li>
+        ))}
       </ul>
     );
   }
